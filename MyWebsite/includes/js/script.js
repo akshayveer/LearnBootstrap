61686ec5418d2d9cb375c10bce8180bb7f8cd0a0
akshayveer <akshayveer096@gmail.com>
2017-06-24 18:55:41 +0530

    Add sample alert container use jquery to show alert change some css of big call out

--- MyWebsite/includes/js/script.js
+++ MyWebsite/includes/js/script.js
@@ -8,3 +8,13 @@ Updated: August 2013
 Notes:	 Hand coded for Udemy.com
 
 */
+
+// this tell jquery to wait till page loads and execute the function
+$(function () {
+  $('#alertMe').click(function (e) {
+    // when we clicks a link it tries to do it's default behaviour
+    // so we are preveting it.
+    e.preventDefault();
+    $('#successAlert').slideDown();
+  })
+});
