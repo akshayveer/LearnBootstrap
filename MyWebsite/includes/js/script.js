c5e06f25a03130fd041044705dc38f15d0b1dbd9
akshayveer <akshayveer096@gmail.com>
2017-06-25 09:59:26 +0530

    code tooltips and popovers

--- MyWebsite/includes/js/script.js
+++ MyWebsite/includes/js/script.js
@@ -17,4 +17,13 @@ $(function () {
     e.preventDefault();
     $('#successAlert').slideDown();
   })
+
+  $('a.pop').click(function (e) {
+    e.preventDefault();
+
+  });
+  $('a.pop').popover();
+
+  $('[rel="tooltip"]').tooltip();
+
 });
